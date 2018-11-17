0ef03ea31a542fc12c9a624d158694bfd369e2a4
cloudr2 <alejandro.popovsky@gmail.com>
2018-11-17 15:25:11 -0300

    Initial commit and made the joystick

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    private VirtualJoystick VJ;
+
+    void Awake() {
+        VJ = FindObjectOfType<VirtualJoystick>();
+    }
+
+    void Update() {
+        float horizontal = VJ.VJHorizontalAxis();
+        float vertical = VJ.VJVerticalAxis();
+
+        Vector3 direction = new Vector3(horizontal,0,vertical);
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+}
